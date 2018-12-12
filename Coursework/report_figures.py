d1aa50a37cb90233c5a9b9981af87a3d11d581f0
Luca Passariello <=>
2018-12-12 01:44:29 +0000

    Improving statistics and graph formatting -- !!!CHECK AVERAGES!!! -- something to do with running 6 races?

--- Coursework/report_figures.py
+++ Coursework/report_figures.py
@@ -3,66 +3,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from operator import itemgetter
 
-def count_wins(num_of_races, file_name = 'values.csv'):
+def count_wins(num_of_models, num_of_races = 6, file_name = 'values.csv'):
     name_list = list(cw.read_sailor_data(file_name).keys())
     # creates a dictionary with the names as keys and empty lists as values
     results = {key:0 for key in name_list}
-    for i in range(0, num_of_races):
-        race_result = cw.run_races(10)
+    for i in range(0, num_of_models):
+        race_result = cw.run_races(num_of_races)
         for index, value in enumerate(race_result):
             results[value] += (index + 1)
     return sorted(results.items(), key=lambda keyvalue: keyvalue[1])
 
-def average_modelling(models_to_average, num_of_races = 1, file_name = 'values.csv'):
-    name_list = list(cw.read_sailor_data(file_name).keys())
-    # creates a dictionary with the names as keys and empty lists as values
-    totals = {key:0 for key in name_list}
-    for i in range(0, models_to_average):
-        current_model = count_wins(num_of_races)
-        for value in current_model:
-            totals[value[0]] += value[1]
-    for key, value in totals.items():
-        totals[key] = value / models_to_average
-    print(sorted(totals.items(), key=itemgetter(1)))
-    return sorted(totals.items(), key=itemgetter(1))
+def average_modelling(num_of_models = 1, file_name = 'values.csv'):
+    result = count_wins(num_of_models)
+    output = {}
+    for tuple in result:
+        output[tuple[0]] = tuple[1] / num_of_models
+    return sorted(output.items(), key=lambda keyvalue: keyvalue[1])
 
-def plot_bar(x_data, y_data, title, x_label, y_label, subplot):
-    index = np.arange(len(x_data))
-    bar_list = plt.bar(index, y_data)
-    for bar in bar_list:
-        bar.set_color('r')
-    plt.xlabel(x_label, fontsize=10)
-    plt.ylabel(y_label, fontsize=10)
-    plt.xticks(index, x_data, fontsize=10, rotation=30)
-    plt.title(title)
-    x = -0.05 # default x value for bar labels
-    for value in y_data:
-        plt.text(x, value + (value * 0.01), value)
-        x += 1
-    plt.show()
 
-race_num = int(input("Input number of models to perform >> "))
+race_num = int(input("Input number of models and averages to perform >> "))
 results = count_wins(race_num)
 print("Over", race_num, "Models")
 for value in results:
     print(value[0], "scored", value[1])
 totals = [value[1] for value in results]
 names = [value[0] for value in results]
-# plot_bar(names, totals, "Total Scores", "Names", "Total Score")
-extra = input("Would you like to see average results? (Y/N) >>")
-if extra.lower() == "y":
-    races_to_average = int(input("How many models should be averaged? >>"))
-    averages = average_modelling(races_to_average)
-    names = [key for key, value in averages]
-    averages = [value for key, value in averages]
-    print(names, averages)
-    avg_averages = []
-    avg_names = []
-    for index in enumerate(names):
-        print(str(names[index[0]]) + "'s average is " + str(averages[index[0]]))
-        avg_names.append(names[index[0]])
-        avg_averages.append(averages[index[0]])
-    # plot_bar(avg_names, avg_averages, "Average Positions", "Names", "Average Position")
+averages = average_modelling(race_num)
+avg_names = [key for key, value in averages]
+avg_values = [value for key, value in averages]
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0] = 10
+fig_size[1] = 8
+print(fig_size)
 def plot_bar(x_data, y_data, title, x_label, y_label, color = "grey"):
     index = np.arange(len(x_data))
     bar_list = plt.bar(index, y_data)
@@ -71,7 +43,7 @@ def plot_bar(x_data, y_data, title, x_label, y_label, color = "grey"):
     plt.xlabel(x_label, fontsize=10)
     plt.ylabel(y_label, fontsize=10)
     plt.xticks(index, x_data, fontsize=10, rotation=30)
-    plt.title(title)
+    plt.title(title, fontsize=20)
     x = -0.05 # default x value for bar labels
     for value in y_data:
         plt.text(x, value * 0.5, value, fontsize=10)
@@ -80,5 +52,6 @@ def plot_bar(x_data, y_data, title, x_label, y_label, color = "grey"):
 plt.subplot(2, 1, 1)
 plot_bar(names, totals, "Total Scores", "Names", "Total Score")
 plt.subplot(2, 1, 2)
-plot_bar(avg_names, avg_averages, "Average Positions", "Names", "Average Positions")
+plot_bar(avg_names, avg_values, "Average Position", "Names", "Average Positions")
+plt.tight_layout()
 plt.show()
